7a6768623aa214f2f2bd1acef67f7f39ce9c35c2
Hyun-Hwan Jeong <jeong.compbio@gmail.com>
2024-12-06 06:15:48 +0000

    solve the P1 in 1205.

---
+++ 1205/P1.ts
@@ -0,0 +1,47 @@
+import * as fs from 'fs';
+import * as readline from 'readline';
+
+const filePath = "input.txt";
+const fileStream = fs.createReadStream(filePath);
+
+const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+});
+
+let answer: number = 0;
+let isFirst: boolean = true;
+const invalid = new Set<string>();
+
+rl.on("line", (line) => {
+    if(line.length==0) {
+        isFirst = false;
+    } else {
+        if(isFirst) {
+            const [a, b] = line.split("|").map(Number);
+            invalid.add(`${b},${a}`);
+            console.log(`${b},${a}`);
+        } else {
+            const numbers = line.split(",").map(Number);
+            let isCorrect: boolean = true;
+
+            for(let i = 0 ; i<numbers.length ; ++i ) {
+                for(let j=i+1 ; j < numbers.length ; ++j) {
+                    console.log(`${numbers[i]},${numbers[j]}`);
+                    if(invalid.has(`${numbers[i]},${numbers[j]}`)) {
+                        isCorrect = false;
+                        break;
+                    }
+                }
+            }
+            console.log(numbers, isCorrect, numbers[Math.floor(numbers.length/2)]);
+            if(isCorrect) {
+                answer += numbers[Math.floor(numbers.length/2)];
+            }    
+        }
+    }
+});
+
+rl.on("close", () => {    
+    console.log(answer);
+});
