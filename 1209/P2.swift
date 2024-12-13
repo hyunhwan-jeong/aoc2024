55838e86523987c1e725fb3231617de21491f165
Hyun-Hwan Jeong <jeong.compbio@gmail.com>
2024-12-12 21:54:16 -0600

    solved P2

--- 1209/P2.swift
+++ 1209/P2.swift
@@ -8,50 +8,75 @@ let diskMap: [Int] = input.map{Int(String($0))!}
 
 var diskId = -1;
 
-var disks: [Int] = []
+var disks : [(start: Int, end: Int, index: Int)] = []
+var spaces : [(start: Int, end: Int)] = []
+
+var diskStatus: [Int] = []
+var n: Int = 0
 
 for (i, steps) in diskMap.enumerated() {
     if i % 2 == 0 {
         diskId += 1
+        disks.append((start: n, end: n+steps, index: diskId))
     }
     if steps == 0 {
         continue
     }
     for _ in 1...steps {
         if i % 2 == 0 {
-            disks.append(diskId)
+            diskStatus.append(diskId)
         } else {
-            disks.append(-1)
+            diskStatus.append(-1)
         }
     }
+    n += steps
 }
 
+print(diskStatus)
+for disk in disks.reversed() {
+    print(disk)
+    let targetCount = disk.end - disk.start 
 
-var l = 0
-var r: Int = disks.count - 1
-
-while l < r {
-    if disks[l] == -1 && disks[r] != -1 {
-        disks.swapAt(l, r)
-        l += 1
-        r -= 1
-    } else {
-        if disks[l] != -1 {
-            l += 1
+    var startIdx: Int? = nil
+    var endIdx: Int? = nil
+    var consecutiveCount = 0
+    for (i, v) in diskStatus.enumerated()  {
+        if i >= disk.start {
+            break
         }
-        if disks[r] == -1 {
-            r -= 1
+        if v == -1 {
+            if consecutiveCount == 0 {
+                startIdx = i
+            }
+            consecutiveCount += 1
+        } else {
+            consecutiveCount = 0
+            startIdx = nil
+        }
+
+        if consecutiveCount >= targetCount {
+            endIdx = i
+            break
         }
     }
+
+    if endIdx != nil {
+        //print(startIdx, endIdx, targetCount, disk)
+        if let startIdx = startIdx, let endIdx = endIdx {
+            var j = disk.end-1
+            for i in startIdx...endIdx {
+                diskStatus.swapAt(i, j)
+                j -= 1
+            }
+        }
+
+    }
+ 
 }
 
 var answer = 0
-
-for (i, v) in disks.enumerated() {
-    if v != -1 {
-        answer += i * v 
-    }
-    
+for (i, v) in diskStatus.enumerated() {
+    answer += i * max(v, 0)
 }
-print(disks)
+
 print(answer)
